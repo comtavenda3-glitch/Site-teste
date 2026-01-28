ad86cc6546a98d556f7d88a021a4c711534fe223
comtavenda3-glitch <comtavenda3@gmail.com>
2026-01-28 11:37:48 -0300

    Create firebase-config.js

---
+++ public/admin/firebase-config.js
@@ -0,0 +1,20 @@
+// Firebase v9+ (modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBo05t4KVpfwF9biqW6DdthsFJAHjrDroE",
+  authDomain: "monety-site-2.firebaseapp.com",
+  projectId: "monety-site-2",
+  storageBucket: "monety-site-2.firebasestorage.app",
+  messagingSenderId: "99984573236",
+  appId: "1:99984573236:web:3dd0f46f383bf8fd211b61"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence);
